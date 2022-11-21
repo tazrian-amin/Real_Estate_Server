c9bdf2c6a1c98af5805ca27950099f99c38ac33c
tazrian-amin <amin.tazrian1979@gmail.com>
2022-11-21 21:28:29 +0600

    added verifyJWT function

--- index.js
+++ index.js
@@ -6,11 +6,30 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
+// Setting Middleware
 app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.nl4sfa8.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'Unauthorized Access!' });
+    }
+
+    const token = authHeader.split(' ')[1];
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden Access!' });
+        }
+
+        req.decoded = decoded;
+        next();
+    })
+}
 
 async function run() {
     try {
