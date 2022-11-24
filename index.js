7a7c2c3bc71d13a4fd3d6688c3f924947cba0802
tazrian-amin <amin.tazrian1979@gmail.com>
2022-11-24 06:51:04 +0600

    loaded specific buying house data using dynamic url with _id

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -44,11 +44,25 @@ async function run() {
             res.send({ token });
         })
 
+        app.get('/overview', async (req, res) => {
+            const query = {};
+            const cursor = buyingCollection.find(query).limit(3);
+            const toBuy = await cursor.toArray();
+            res.send(toBuy);
+        })
+
         app.get('/buy', async (req, res) => {
             const query = {};
             const cursor = buyingCollection.find(query);
             const toBuy = await cursor.toArray();
             res.send(toBuy);
+        })
+
+        app.get('/buy/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const details = await buyingCollection.findOne(query);
+            res.send(details);
         })
     }
     finally {
